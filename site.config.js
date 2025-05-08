6a07c2c0d02ef6cd24de4d7cf9823b745f5666f3
Anthony Sandesh <52021528+anthonysandesh@users.noreply.github.com>
2025-05-08 09:20:31 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `mybraincells`,
+      href: "https://github.com/anthonysandesh/mybraincells",
     },
   ],
   // blog setting (required)
@@ -25,7 +25,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://mybraincells.vercel.app",
-  since: 2023, // If leave this empty, current year will be used.
+  since: "2023", // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
